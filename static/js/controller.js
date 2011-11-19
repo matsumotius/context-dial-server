71a33bec89db4385dfbfabccaec18b20aafbf280
myatsumoto <myatsumoto@gmail.com>
2011-11-20 05:28:37 +0900

    post message when value diff > 1

--- static/js/controller.js
+++ static/js/controller.js
@@ -5,6 +5,7 @@ $(function(){
     cds.user_id = $('#user_id').text();
     cds.host = $('#host').text();
     cds.enter = ['root', 'video'];
+    cds.context = { sound : 0, video : 0, time : 0 };
     // socket
     var socket = io.connect(cds.host);
     socket.emit('join', { type : 'controller', id : cds.user_id });
@@ -24,7 +25,9 @@ $(function(){
     context_dial.set_image('time', { url : '/image/media-player.png', width : 80, height : 80 });
     context_dial.set_image('video', { url : '/image/videos.png', width : 80, height : 80 });
     context_dial.on('change', function(context){
-        socket.emit('change', { key : context.key, value : context.value });
+        if(context.key in cds.context || Math.abs(cds.context[context.key] - context.value) < 1) return;
+        cds.context[context.key] = context.value;
+        socket.emit('change', { key : context.key, value : parseInt(context.value) });
     });
     context_dial.on('touchend', function(context){
         if(cds.enter.indexOf(context.key) < 0) return;
