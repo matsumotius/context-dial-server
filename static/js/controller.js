f3472f5de71e99c81d734327cf6a61427d6741b3
myatsumoto <myatsumoto@gmail.com>
2011-11-21 18:29:16 +0900

    remove history

--- static/js/controller.js
+++ static/js/controller.js
@@ -19,7 +19,6 @@ $(function(){
             cds.mode = 'video';
             context_dial.remove('link');
             context_dial.remove('scroll');
-            context_dial.remove('history');
             context_dial.add('sound', { max : 100, min : 0, value_by_rot : 50, en : en_options });
             context_dial.add('time', { max : 100, min : 0, value_by_rot : 50, en : en_options });
             context_dial.add('video', { max : 100, min : 0, value_by_rot : 50, en : en_options });
@@ -33,10 +32,8 @@ $(function(){
             context_dial.remove('video');
             context_dial.add('link', { max : 9999, min : 0, value_by_rot : 50, en : en_options });
             context_dial.add('scroll', { max : 9999, min : 0, value_by_rot : 50, en : en_options });
-            context_dial.add('history', { max : 100, min : -100, value_by_rot : 25, en : en_options });
             context_dial.set_image('link', { url : '/image/internet.png', width : 80, height : 80 });
             context_dial.set_image('scroll', { url : '/image/scroll.png', width : 80, height : 80 });
-            context_dial.set_image('history', { url : '/image/media-player.png', width : 80, height : 80 });
         }
     });
     // context-dial
@@ -46,10 +43,8 @@ $(function(){
     var en_options = { x : 0, y : 0, radius : 80, type : 'fill', color : '#ff8c00' };
     context_dial.add('link', { max : 9999, min : 0, value_by_rot : 50, en : en_options });
     context_dial.add('scroll', { max : 9999, min : 0, value_by_rot : 50, en : en_options });
-    context_dial.add('history', { max : 100, min : -100, value_by_rot : 25, en : en_options });
     context_dial.set_image('link', { url : '/image/internet.png', width : 80, height : 80 });
     context_dial.set_image('scroll', { url : '/image/scroll.png', width : 80, height : 80 });
-    context_dial.set_image('history', { url : '/image/media-player.png', width : 80, height : 80 });
     context_dial.on('change', function(context){
         socket.emit('change', { to : cds.mode, key : context.key, value : parseInt(context.value) });
     });
